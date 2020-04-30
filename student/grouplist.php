a3b7268f9eaddb5a5c93edf919719e418c5c9645
Black-Sinatra <afolabiolatoye@gmail.com>
2020-05-01 00:04:08 +0100

    Final Application

--- student/grouplist.php
+++ student/grouplist.php
@@ -4,45 +4,47 @@ include("includes/header.php")
 //include_once("db.php");
 ?>
     
-    <div class="wrapper d-flex align-items-stretch">
+<div class="wrapper d-flex align-items-stretch">
+
+    <?php
+    include("includes/navbar.php");
+    ?>
+
+    <div id="content" class="p-4 p-md-5">
 
         <?php
-        include("includes/navbar.php");
-        ?>  
+        include("includes/content.php");
+        ?>
 
-        <div id="content" class="p-4 p-md-5">
-            
+        <div class="row">
+
             <?php
-            include("includes/content.php");
-            ?>
+            $letters = array('A', 'B', 'C', 'D', 'E', 'F');
 
-            <div class="row">
-                
-                <?php
-                $letters = array('A', 'B', 'C', 'D', 'E', 'F');
-                
-                foreach ($letters as $group_name) {
-                    $sqlQuery = "SELECT name FROM group$group_name";
-                    $resultSet = mysqli_query($conn, $sqlQuery) or die("database error:". mysqli_error($conn));
-                    $developer = mysqli_fetch_assoc($resultSet);?>
-                        <div class="col-sm-4" style="padding: 10px;">
-                            <div class="card" style="width: 18rem;">
-                                <div class="card-header">
-                                    Group <?php echo $group_name;?>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item"><?php echo $developer ['name'];?></li>
-                                </ul>
+            foreach ($letters as $group_name) {
+                $sqlQuery = "SELECT * FROM group$group_name";
+                $resultSet = mysqli_query($conn, $sqlQuery) or die("database error:". mysqli_error($conn));
+                ?>
+                    <div class="col-sm-4" style="padding: 10px;">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-header">
+                                Group <?php echo $group_name;?>
                             </div>
+                            <ul class="list-group list-group-flush">
+                                <?php while($developer = mysqli_fetch_assoc($resultSet)){?>
+                                <li class="list-group-item"><?php echo $developer ['name'];?></li>
+                                <?php };?>
+                            </ul>
                         </div>
-                   <?php }?>
-                
-            </div>
+                    </div>
+               <?php }?>
 
         </div>
 
     </div>
 
-    <?php
-    include("includes/footer.php");
-    ?>
+</div>
+
+<?php
+include("includes/footer.php");
+?>
